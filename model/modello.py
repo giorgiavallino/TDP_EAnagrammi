c46c8b13d3a5667c132d04d96061041be7cec00e
s298157 <s298157@studenti.polito.it>
2025-05-03 12:51:42 +0200

    Update

--- model/modello.py
+++ model/modello.py
@@ -1,3 +1,16 @@
 class Model:
+
     def __init__(self):
         pass
+
+    def calcola_anagrammi(self, parola: str):
+        self._ricorsione("", parola)
+
+    def _ricorsione(self, soluzione_parziale, lettere_rimanenti):
+        if len(lettere_rimanenti) == 0:
+            return print(soluzione_parziale)
+        else:
+            for i in range(len(lettere_rimanenti)):
+                soluzione_parziale = soluzione_parziale + lettere_rimanenti[i]
+                nuove_lettere_rimanenti = lettere_rimanenti[:,i] + lettere_rimanenti[i+1,:]
+
